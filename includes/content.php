66a6967effb782317febeb514139540e15dab758
Nguyen Phuoc Thinh <npthinh1996@gmail.com>
2018-12-07 22:26:34 +0700

    add 'text-center'

--- includes/content.php
+++ includes/content.php
@@ -1,8 +1,8 @@
-<div style="width:182px;display:inline-block;">
+<div style="width:182px;display:inline-block">
 <div class="card mb-4">
     <?php the_post_thumbnail('blog-thumbnail',  ['class' => 'card-img-top', 'alt' => 'post-image']) ?>
     <div class="card-body">
-        <h2 class="card-title"><?php the_title() ?></h2>
+        <h2 class="card-title text-center"><?php the_title() ?></h2>
         <p class="card-text"><?php the_excerpt() ?></p>
         <a href="<?php the_permalink() ?>" class="btn btn-primary">Xem thêm &rarr;</a>
     </div>
